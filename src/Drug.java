4b83afcb4678a75f6be1f12fd91ce2400ffa0dbc
Mahmoud mohamed <143263824+MahmoudCodeMaster@users.noreply.github.com>
2024-05-13 23:39:04 +0300

    Add files via upload
    sorry , I upload the latest version of drug class , i publish now the current version of drug class

--- src/Drug.java
+++ src/Drug.java
@@ -1,8 +1,11 @@
+
+
 public class Drug {
     private String id;
     private String name;
     private double price;
     private String category;
+    private double totalPrice;
     private int availableQuantity;
 
     public Drug(String id, String name, double price, String category, int availableQuantity) {
@@ -18,6 +21,11 @@ public class Drug {
     public String getName() { return name; }
     public double getPrice() { return price; }
     public String getCategory() { return category; }
+    public double getTotalPrice()
+    {
+        return totalPrice;}
+
     public int getAvailableQuantity() { return availableQuantity; }
     public void setAvailableQuantity(int availableQuantity) { this.availableQuantity = availableQuantity; }
 }
+
